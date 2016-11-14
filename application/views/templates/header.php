b729e85f366c9523bd01445cf8bda2be9dfb70c1
Jules Esnault <julesnault@gmail.com>
2016-11-14 14:59:57 +0100

    favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,14 +4,32 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SkillDev - <?php echo $title; ?></title>
+    <title>DevClub - <?php echo $title; ?></title>
 
+    <link rel="manifest" href="<?php echo base_url(); ?>icons/manifest.json">
+    <meta name="msapplication-TileColor" content="#3e3f3a">
+    <meta name="msapplication-TileImage" content="<?php echo base_url(); ?>icons/ms-icon-144x144.png">
+    <meta name="theme-color" content="#3e3f3a">
     <!-- Bootstrap -->
     <link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>css/monokai.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>css/dropzone.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>css/c3.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url(); ?>icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url(); ?>icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url(); ?>icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url(); ?>icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url(); ?>icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url(); ?>icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url(); ?>icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url(); ?>icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?php echo base_url(); ?>icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url(); ?>icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo base_url(); ?>icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>icons/favicon-16x16.png">
 
 </head>
 <body>
